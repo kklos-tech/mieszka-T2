a03a0aa88334386dca8f9c548c38c7d00ad7a703
kklos-tech <kklos@studioniepokoj.pl>
2026-03-23 12:33:40 +0100

    Fix Polish diacritics

--- src/projekty-data.js
+++ src/projekty-data.js
@@ -3,7 +3,7 @@ export const projektyData = [
     id: 1,
     title: "Łazienka marzeń",
     type: "Kamienica",
-    size: "7m\u00B2",
+    size: "7m²",
     date: "2015",
     images: [
       "/images/projects/001/1.jpg",
@@ -12,16 +12,16 @@ export const projektyData = [
       "/images/projects/001/4.jpg",
       "/images/projects/001/5.jpg"
     ],
-    challenge:"Stan deweloperski. Ze wzgl\u0119du na naro\u017Cny uk\u0142ad kamienicy zastany jest nietypowy uk\u0142ad \u015Bcian. Punkty medi\u00F3w narzucaj\u0105 niekorzystny uk\u0142ad dla projektu pomieszczenia. Prysznic mia\u0142 powsta\u0107 w nietypowym miejscu, technicznie trudnym do realizacji. Dwie mokre strefy.",
-    solution: "Przestawione \u015Bcianki dzia\u0142owe i punkty medi\u00F3w. Niewidoczne podniesienie pod\u0142ogi w celu ukrycia rury odp\u0142ywowej. Hydraulika zaprojektowana na milimetry.",
-    result: "Przestrze\u0144 zyska\u0142a logik\u0119 i komfort u\u017Cytkowania. Zmie\u015Bci\u0142 si\u0119 prysznic marze\u0144 w oczekiwanym miejscu. Podniesiona pod\u0142oga stanowi cz\u0119\u015B\u0107 projektowanego odp\u0142ywu. Zabieg pom\u00F3g\u0142 ukszta\u0142towa\u0107 tak\u017Ce dodatkowe pomieszczenia, poza projektowan\u0105 \u0142azienk\u0105: Powsta\u0142a mini garderoba i sensowny uk\u0142ad kuchni. 'Na deser' zosta\u0142a zaprojektowana mozaika (w stylu ilustracji Charley Harper'a)",
-    benefits: ["Praktyczny podzia\u0142 na strefy", "Du\u017Co przestrzeni", "Pokonane przeszkody techniczne"]
+    challenge: "Stan deweloperski. Ze względu na narożny układ kamienicy zastany jest nietypowy układ ścian. Punkty mediów narzucają niekorzystny układ dla projektu pomieszczenia. Prysznic miał powstać w nietypowym miejscu, technicznie trudnym do realizacji. Dwie mokre strefy.",
+    solution: "Przestawione ścianki działowe i punkty mediów. Niewidoczne podniesienie podłogi w celu ukrycia rury odpływowej. Hydraulika zaprojektowana na milimetry.",
+    result: "Przestrzeń zyskała logikę i komfort użytkowania. Zmieścił się prysznic marzeń w oczekiwanym miejscu. Podniesiona podłoga stanowi część projektowanego odpływu. Zabieg pomógł ukształtować także dodatkowe pomieszczenia, poza projektowaną łazienką: Powstała mini garderoba i sensowny układ kuchni. 'Na deser' została zaprojektowana mozaika (w stylu ilustracji Charley Harper'a)",
+    benefits: ["Praktyczny podział na strefy", "Dużo przestrzeni", "Pokonane przeszkody techniczne"]
   },
   {
     id: 2,
     title: "Budżetowa kuchnia",
     type: "Stare budownictwo",
-    size: "40m\u00B2",
+    size: "40m²",
     date: "2025",
     images: [
       "/images/projects/002/1.jpg",
@@ -29,36 +29,36 @@ export const projektyData = [
       "/images/projects/002/3.jpg",
       "/images/projects/002/4.jpg"
     ],
-    challenge:"Niewielka kuchnia, wymagająca remontu. Ograniczona ścianami nośnymi. Punkty wody i pralka muszą zostać na swoim miejscu. Bardzo niski budżet. Niewielka przestrzeń robocza. Pralka musi zostać. Odzyskiwanie starych materiałów i mebli. Odmalowanie kafli",
-    result: "Przestrze\u0144 zyska\u0142a logik\u0119 i komfort u\u017Cytkowania. Du\u017Co przestrzeni roboczej dzi\u0119ki wielkim blatom. Kompromisem by\u0142 ma\u0142y st\u00F3\u0142, ale korzystaj\u0105 z niego tylko dwie osoby.",
-    benefits: ["Szybka realizacja", "Niski koszt", "Du\u017Co przestrzeni roboczej"]
+    challenge: "Niewielka kuchnia, wymagająca remontu. Ograniczona ścianami nośnymi. Punkty wody i pralka muszą zostać na swoim miejscu. Bardzo niski budżet. Niewielka przestrzeń robocza. Pralka musi zostać. Odzyskiwanie starych materiałów i mebli. Odmalowanie kafli",
+    result: "Przestrzeń zyskała logikę i komfort użytkowania. Dużo przestrzeni roboczej dzięki wielkim blatom. Kompromisem był mały stół, ale korzystają z niego tylko dwie osoby.",
+    benefits: ["Szybka realizacja", "Niski koszt", "Dużo przestrzeni roboczej"]
   },
   {
     id: 3,
     title: "Domek szeregowy",
     type: "Jednorodzinny",
-    size: "120m\u00B2",
+    size: "120m²",
     date: "2025",
     images: [
       "/images/projects/003/1.jpg"
     ],
-    challenge:"Przestronny dom z chaotycznym uk\u0142adem funkcjonalnym i niewykorzystanym potencja\u0142em elewacji po\u0142udniowo-zachodniej. Nieprawid\u0142owo zaprojektowana strefa dzienna i jej do\u015Bwietlenie. W\u0105tpliwo\u015Bci co do mo\u017Cliwo\u015Bci i zasadno\u015Bci dobudowania wiaty gara\u017Cowej w przysz\u0142o\u015Bci.",
-    solution: "Przeprojektowali\u015Bmy analiz\u0119 potrzeb klienta. Policzona zosta\u0142a ergonomia strefy dziennej i jej okien. Powsta\u0142 nowy uk\u0142ad \u015Bcian dzia\u0142owych w kilku pomieszczeniach.",
-    result: "Przestrze\u0144 zyska\u0142a logik\u0119 i komfort u\u017Cytkowania. Na g\u00F3rze przedpok\u00F3j jest kr\u00F3tszy a pomieszczenia wi\u0119ksze. \u0141azienka zyska\u0142a po\u0142udniowe okno. Pok\u00F3j dziecka jest bezwzgl\u0119dnie lepiej u\u0142o\u017Cony i r\u00F3wnomiernie do\u015Bwietlony.",
-    benefits: ["Wydajne do\u015Bwietlenie pomieszcze\u0144", "Lepsza organizacja przestrzeni do \u017Cycia", "Gara\u017C bez kompromis\u00F3w na jako\u015Bci"]
+    challenge: "Przestronny dom z chaotycznym układem funkcjonalnym i niewykorzystanym potencjałem elewacji południowo-zachodniej. Nieprawidłowo zaprojektowana strefa dzienna i jej doświetlenie. Wątpliwości co do możliwości i zasadności dobudowania wiaty garażowej w przyszłości.",
+    solution: "Przeprojektowaliśmy analizę potrzeb klienta. Policzona została ergonomia strefy dziennej i jej okien. Powstał nowy układ ścian działowych w kilku pomieszczeniach.",
+    result: "Przestrzeń zyskała logikę i komfort użytkowania. Na górze przedpokój jest krótszy a pomieszczenia większe. Łazienka zyskała południowe okno. Pokój dziecka jest bezwzględnie lepiej ułożony i równomiernie doświetlony.",
+    benefits: ["Wydajne doświetlenie pomieszczeń", "Lepsza organizacja przestrzeni do życia", "Garaż bez kompromisów na jakości"]
   },
   {
     id: 4,
     title: "Kamienica w Poznaniu",
     type: "Mieszkanie",
-    size: "80m\u00B2",
+    size: "80m²",
     date: "2021",
     images: [
       "/images/projects/004/1.jpg"
     ],
-    challenge:"Rodzina z dw\u00F3jk\u0105 dzieci zmaga\u0142a si\u0119 z ci\u0105g\u0142ym ba\u0142aganem i brakiem miejsca na rzeczy codziennego u\u017Cytku. Pomieszczenia by\u0142y \u017Ale zorganizowane - du\u017Cy salon by\u0142 rzadko u\u017Cywany, podczas gdy strefa dzienna by\u0142a zbyt ma\u0142a.",
-    solution: "Przeprojektowali\u015Bmy strefy funkcjonalne domu. Stworzy\u0142i\u015Bmy wi\u0119ksz\u0105 i bardziej praktyczn\u0105 kuchni\u0119 z jadalni\u0105. Zaprojektowali\u015Bmy dedykowan\u0105 przestrze\u0144 do nauki dla dzieci i dodali\u015Bmy inteligentne systemy przechowywania w ka\u017Cdej strefie. Dodali\u015Bmy przestrzenie magazynowe z niucz\u0119szczanych stref komunikacji.",
-    result: "Mieszkanie sta\u0142o si\u0119 przyjazne dla ca\u0142ej rodziny. Ka\u017Cdy cz\u0142onek rodziny ma swoj\u0105 przestrze\u0144, a r\u00F3wnocze\u015Bnie strefa wsp\u00F3lna sprzyja sp\u0119dzaniu czasu razem. W\u0142a\u015Bciciele podkre\u015Blaj\u0105, \u017Ce wreszcie czuj\u0105 si\u0119 komfortowo we w\u0142asnym domu.",
-    benefits: ["Lepsze zarz\u0105dzanie przestrzeni\u0105", "Wi\u0119cej miejsca do \u017Cycia", "Zadowolona rodzina"]
+    challenge: "Rodzina z dwójką dzieci zmagała się z ciągłym bałaganem i brakiem miejsca na rzeczy codziennego użytku. Pomieszczenia były źle zorganizowane - duży salon był rzadko używany, podczas gdy strefa dzienna była zbyt mała.",
+    solution: "Przeprojektowaliśmy strefy funkcjonalne domu. Stworzyliśmy większą i bardziej praktyczną kuchnię z jadalnią. Zaprojektowaliśmy dedykowaną przestrzeń do nauki dla dzieci i dodaliśmy inteligentne systemy przechowywania w każdej strefie. Dodaliśmy przestrzenie magazynowe z nieuczęszczanych stref komunikacji.",
+    result: "Mieszkanie stało się przyjazne dla całej rodziny. Każdy członek rodziny ma swoją przestrzeń, a równocześnie strefa wspólna sprzyja spędzaniu czasu razem. Właściciele podkreślają, że wreszcie czują się komfortowo we własnym domu.",
+    benefits: ["Lepsze zarządzanie przestrzenią", "Więcej miejsca do życia", "Zadowolona rodzina"]
   }
 ];
